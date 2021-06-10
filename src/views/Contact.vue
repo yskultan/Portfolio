e8c976bcf2cfcbda77e8ee45c398c3e65c1bf7d0
skultan <y.skultan@gmail.com>
2021-06-10 18:15:04 +0700

    remove form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,29 +5,9 @@
         <div class="my-8">
             <span class="text-info text-xl my-8">
                 {{ contactsPage[language].description}}
-            </span><br>
-            <form method="POST" data-netlify="true" data-netlify-honeypot="bot-field" name="contact" class="p-6 mt-6 sm:w-100 md:w-1/2 ml-auto mr-auto flex flex-col justify-center">
-                <input type="hidden" name="form-name" value="contact">
-                <div class="flex flex-col mt-2">
-                    <input type="email" name="email" placeholder="Email" class="w-100 mt-2 py-3 px-3 rounded-sm bg-background border-2 border-accent-transparent text-info focus:border-secondary focus:outline-none">
-                </div>
-                <div class="flex flex-col mt-2">
-                    <input type="name" name="name" placeholder="Full Name" class="w-100 mt-2 py-3 px-3 rounded-sm bg-background border-2 border-accent-transparent text-info focus:border-secondary focus:outline-none">
-                </div>
-                <div class="flex flex-col mt-2">
-                    <textarea v-model="mes" placeholder="Message" class="w-100 h-32 mt-2 py-3 px-3 rounded-sm bg-background border-2 border-accent-transparent text-info focus:border-secondary focus:outline-none">
-                    </textarea>
-                </div>
-                <button type="submit" class="sm:w-100 md:w-1/2 ml-auto mr-auto my-6 rounded-sm text-lg p-4 border-2 text-secondary hover:bg-secondary-transparent">
-                    {{ contactsPage[language].label }}
-                </button>
-            </form>
+            </span>
         </div>
         <div class="my-8">
-            <span class="text-info text-xl my-8">
-                {{ contactsPage[language].subtitle }}
-            </span>
-            <br>
             <div class="flex flex-wrap justify-center mt-4">
                 <div v-for="item in socials" v-bind:key="item.name" class="flex-grow m-2 sm:mx-0 sm:my-4 sm:w-auto md:mx-2 bg-background-light rounded-sm">
                     <a :href="item.link" class="p-4 text-center rounded-sm text-xl text-info hover:text-secondary  hover:bg-secondary-transparent block">
